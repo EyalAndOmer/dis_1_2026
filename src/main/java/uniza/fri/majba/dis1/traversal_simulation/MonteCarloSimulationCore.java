4eeb349e0228f6042290c7b4698c03e742a5f111
klacek <maros.majba2551@gmail.com>
2026-02-21 13:08:27 +0100

    Added implementation of the simulation core

---
+++ src/main/java/uniza/fri/majba/dis1/traversal_simulation/MonteCarloSimulationCore.java
@@ -0,0 +1,10 @@
+package uniza.fri.majba.dis1.traversal_simulation;
+
+import uniza.fri.majba.dis1.simulation_core.Replication;
+import uniza.fri.majba.dis1.simulation_core.SimulationCore;
+
+public final class MonteCarloSimulationCore extends SimulationCore {
+    public MonteCarloSimulationCore(Replication replication) {
+        super(replication);
+    }
+}
---
+++ src/main/java/uniza/fri/majba/dis1/traversal_simulation/TraversalReplication.java
@@ -0,0 +1,30 @@
+package uniza.fri.majba.dis1.traversal_simulation;
+
+import uniza.fri.majba.dis1.simulation_core.Replication;
+
+public class TraversalReplication implements Replication {
+    @Override
+    public void beforeAllReplications() {
+
+    }
+
+    @Override
+    public void afterAllReplications() {
+
+    }
+
+    @Override
+    public void beforeReplication() {
+
+    }
+
+    @Override
+    public void afterReplication() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
